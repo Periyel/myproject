f9d600ec32e4f82894a140c713a5547f7c921973
Periyel <117493704+Periyel@users.noreply.github.com>
2026-08-19 12:49:25 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navigation = document.querySelector(".navigation");
-
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navigation.classList.toggle("active");
-  });
-};
